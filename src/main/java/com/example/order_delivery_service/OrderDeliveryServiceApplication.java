3ef0ea29705d37326ac985bf5f73fcea8b311412
junghyunPark <wjdguswjdal@naver.com>
2025-04-02 14:46:39 +0900

    Initial commit

---
+++ src/main/java/com/example/order_delivery_service/OrderDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.order_delivery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/order_delivery_service/OrderDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.order_delivery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
